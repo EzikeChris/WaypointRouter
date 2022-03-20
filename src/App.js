6f1098f6857d51d46e8d0d8f086d1d9961d4bc15
EzikeBAYC <synergyaffirm@gmail.com>
2022-03-20 23:39:52 +0100

    Feat: Project Setup

--- src/App.js
+++ src/App.js
@@ -25,35 +25,35 @@ import { useRef, useState } from "react";
 const center = { lat: 9.0765, lng: 7.3986 };
 
 function App() {
+  // loads goggle map and auto complete //
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.FAI_API_KEY,
     libraries: ["places"],
   });
 
+  // Creates hooks for  Map, Direction, Distance & Duration
   const [map, setMap] = useState(/** @type google.maps.Map */ (null));
   const [directionsResponse, setDirectionsResponse] = useState(null);
   const [distance, setDistance] = useState("");
   const [duration, setDuration] = useState("");
-
-  /** @type React.MutableRefObject<HTMLInputElement> */
   const originRef = useRef();
-  /** @type React.MutableRefObject<HTMLInputElement> */
   const destiantionRef = useRef();
 
+  // creates skeleton if map not loaded //
   if (!isLoaded) {
-    return <SkeletonText />;
+    return <SkeletonText startColor="pink.500" endColor="orange.500" />;
   }
 
+  // Calculate Route function
   async function calculateRoute() {
     if (originRef.current.value === "" || destiantionRef.current.value === "") {
       return;
     }
-    // eslint-disable-next-line no-undef
+
     const directionsService = new google.maps.DirectionsService();
     const results = await directionsService.route({
       origin: originRef.current.value,
       destination: destiantionRef.current.value,
-      // eslint-disable-next-line no-undef
       travelMode: google.maps.TravelMode.DRIVING,
     });
     setDirectionsResponse(results);
@@ -61,6 +61,7 @@ function App() {
     setDuration(results.routes[0].legs[0].duration.text);
   }
 
+  // Clear Route function
   function clearRoute() {
     setDirectionsResponse(null);
     setDistance("");
@@ -74,18 +75,19 @@ function App() {
       position="relative"
       flexDirection="column"
       alignItems="center"
+      bgImage="https://images.unsplash.com/photo-1637288099468-93ae6e3124e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1800&q=80"
       h="100vh"
       w="100vw"
     >
-      <Heading as="h1" fontSize="30px" color="green">
-        Find The Distance Between Two Locations
+      <Heading as="h1" fontSize="30px">
+        Search The Distance Between Two Locations
       </Heading>
-      <Text fontSize="" color="green">
-        Calculate Your Travelling Distance
+      <Text fontSize="" color="red">
+        Calculate Your Travelling Route
       </Text>
 
       <Box position="absolute" left={60} top={60} h="60%" w="80%">
-        {/* Google Map Box */}
+        {/* Creates Marker & Direction*/}
         <GoogleMap
           center={center}
           zoom={12}
@@ -115,6 +117,7 @@ function App() {
       >
         <HStack spacing={2} justifyContent="space-between">
           <Box flexGrow={1}>
+            {/* wraps input with auto Complete component*/}
             <Autocomplete>
               <Input
                 type="text"
@@ -147,6 +150,8 @@ function App() {
         <HStack spacing={4} mt={4} justifyContent="space-between">
           <Text>Distance: {distance} </Text>
           <Text>Duration: {duration} </Text>
+
+          {/* Reset Map to Position */}
           <IconButton
             aria-label="center back"
             icon={<FaLocationArrow />}
